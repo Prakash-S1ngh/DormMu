53b73213031129264af0af76434e593e5e960e53
Prakash-S1ngh <prakashsingh14082002@gmail.com>
2025-10-04 16:00:46 +0530

    Adding DockerFiles to it

--- Hostel_Management_Backend/src/utils/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet,Navigate } from 'react-router-dom'
-import { useAuth } from '../services/AuthProvider';
-const ProtectedRoutes = ()=>{
-   const {user}=useAuth();
-    const resident = user;
-    return resident ? <Outlet/> : <Navigate to="/"/>;
-}
-export default ProtectedRoutes;
